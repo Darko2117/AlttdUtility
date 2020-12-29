d83d52241d632d56a97497531dfa07a6780d9478
Len <40720638+destro174@users.noreply.github.com>
2020-12-29 13:57:23 +0100

    Add public containers

---
+++ src/main/java/com/darko/main/utilities/griefprevention/PublicChests.java
@@ -0,0 +1,47 @@
+package com.darko.main.utilities.griefprevention;
+
+import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import org.bukkit.Nameable;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.InventoryHolder;
+
+/**
+ * https://discordapp.com/channels/141644560005595136/677219092717109289/788689097064579083
+ * adapted to be for all chests, barrels and shulkers
+ */
+public class PublicChests {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onRaidStart(PlayerInteractEvent event) {
+        //if (!Main.getInstance().getConfig().getBoolean("FeatureToggles.AllowNamedPublicChests")) return;//TODO add this toggle
+
+        if (event.isCancelled() && event.hasBlock()) {
+            Block block = event.getClickedBlock();
+            Player player = event.getPlayer();
+            /*
+             * This should cover all chests, enderchests, barrels, furnacetypes, lectern, ....
+             */
+            if(block.getState() instanceof InventoryHolder) {
+                // Do we need to check for claims? i assume it's cancelled because of no permission as we only continue if the event is canceled
+                // we don't need to check for worldguard regions either as spawn should also be claimed?
+                Claim claim = GriefPrevention.instance.dataStore.getClaimAt(block.getLocation(), true, null);
+                if (claim != null) {
+                    if (claim.allowAccess(player) != null) {
+                        if (block.getState() instanceof Nameable) {
+                            Nameable nameable = (Nameable) block.getState();
+                            if (nameable.getCustomName().equalsIgnoreCase("public")) {
+                                event.setCancelled(false);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
