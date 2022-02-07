05196fc29e6e64cc07c603649009f66c8447a1d4
Darko <darkohorvatovic@gmail.com>
2022-02-07 17:37:27 +0100

    Minor changes

--- src/main/java/com/darko/main/common/BukkitTasksCache.java
+++ src/main/java/com/darko/main/common/BukkitTasksCache.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class BukkitTasksCache {
 
-    static List<BukkitTask> tasksToCancel = new ArrayList<>();
+    private static final List<BukkitTask> tasksToCancel = new ArrayList<>();
 
     public static void cancelRunningTasks() {
 
