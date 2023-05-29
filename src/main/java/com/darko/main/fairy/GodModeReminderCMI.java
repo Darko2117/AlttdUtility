323639a34f7217e3017815c7e5e98cbb986b260c
once_a_fairy <faaairyhome@gmail.com>
2023-05-29 10:19:52 +0200

    Adds a reminder when the CMI godmode is turned on. Applied some of Teri's suggestions.

--- src/main/java/com/darko/main/fairy/GodModeReminderCMI.java
+++ src/main/java/com/darko/main/fairy/GodModeReminderCMI.java
@@ -10,29 +10,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class GodModeReminderCMI {
-    private static long lastSentReminder;
-    private static int delay;
+    private static final int delay = AlttdUtility.getInstance().getConfig().getInt("GodModeReminderDelay");
 
     public static void initiate() {
 
         CMI cmiAPI = APIs.getCMIAPI();
 
-        delay = AlttdUtility.getInstance().getConfig().getInt("GodModeReminder.Delay");
         BukkitTasksCache.addTask(new BukkitRunnable() {
             @Override
             public void run() {
 
-        if (System.currentTimeMillis() < lastSentReminder + delay)
-            return;
-
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (cmiAPI.getPlayerManager().getUser(player).isGod()) {
-                if (player == null) return;
-                Methods.sendConfigMessage(player, "Messages.GodModeReminder");
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                if (cmiAPI.getPlayerManager().getUser(player).isGod()) {
+                    Methods.sendConfigMessage(player, "Messages.GodModeReminder");
+                }
             }
-        }
-                lastSentReminder = System.currentTimeMillis();
             }
-        }.runTaskTimerAsynchronously(AlttdUtility.getInstance(), 200, 200));
+        }.runTaskTimer(AlttdUtility.getInstance(), delay, delay)); //using non-asynchronous task timer because APIs should never be accessed by them
     }
 }
